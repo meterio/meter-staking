43b8f92a6fc52546852471bcc45c11114a2b2b45
yanfeizuo <18856856693@163.com>
2022-04-11 19:17:15 +0800

    getBuckets when init coz get maxVotes need buckets

--- src/store/modules/wallet.js
+++ src/store/modules/wallet.js
@@ -65,6 +65,8 @@ const actions = {
 
     await dispatch('token/initTokens', null, { root: true })
 
+    await dispatch('bucket/getBuckets', null, { root: true })
+
     dispatch('candidate/getCandidates', null, { root: true })
     dispatch('bailout/getJaileds', null, { root: true })
   },

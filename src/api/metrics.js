63937d6ad7afe8e879a0ed3793557e0a46a88b49
yanfeizuo <18856856693@163.com>
2025-04-23 11:45:05 +0800

    instead api.meter.io:8000 by api-scan.meter.io

--- src/api/metrics.js
+++ src/api/metrics.js
@@ -1,7 +1,8 @@
 import axios from "axios"
+import { Base_Url } from "../constants"
 
-export const getMetricsAll = async (url) => {
-  const res = await axios.get(url)
+export const getMetricsAll = async () => {
+  const res = await axios.get(`${Base_Url}/api/metrics/all`)
   if (res && res.data) {
     return res.data
   }
